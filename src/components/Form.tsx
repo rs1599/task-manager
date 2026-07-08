ba4f4b0ccdcb0fd692c38087a4c8445203ff35a8
rs1599 <rs.gumi0615@gmail.com>
2026-07-08 22:08:05 +0900

    add layout

---
+++ src/components/Form.tsx
@@ -0,0 +1,10 @@
+function TaskForm() {
+  return (
+    <section>
+      <h2>タスク追加</h2>
+      <p>テスト</p>
+    </section>
+  );
+}
+
+export default TaskForm;
---
+++ src/components/List.tsx
@@ -0,0 +1,10 @@
+function TaskList() {
+  return (
+    <section>
+      <h2>タスク一覧</h2>
+      <p>テスト</p>
+    </section>
+  );
+}
+
+export default TaskList;
